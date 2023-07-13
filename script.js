2361bb7ecd45411ab42ecfc9e0143f6b28561fe4
Redsnow09 <131536942+Redsnow09@users.noreply.github.com>
2023-07-13 22:23:22 +0300

    2

---
+++ script.js
@@ -0,0 +1,10 @@
+multiply(); 
+function multiply() {
+  // Тело функции
+  console.log("Hello, this is my first function!");
+}
+
+// 2. Вызовы функции multiply
+multiply(sayHi); // multiply function invocation
+multiply(sayHi); // multiply function invocation
+multiply(sayHi); // multiply function invocation
