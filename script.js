10a0e832c68287986756df3f7da4f6a972c8fd90
Redsnow09 <131536942+Redsnow09@users.noreply.github.com>
2023-07-14 22:43:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -55,15 +55,24 @@
 
 
 
-function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
-  let totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
-  let message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
-  return message;
-}
+//function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//  let totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
+//  let message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+//  return message;
+//}
 
-console.log(makeOrderMessage(2, 100, 50));
-console.log(makeOrderMessage(4, 300, 100));
-console.log(makeOrderMessage(10, 70, 200));
+//console.log(makeOrderMessage(2, 100, 50));
+//console.log(makeOrderMessage(4, 300, 100));
+//console.log(makeOrderMessage(10, 70, 200));
+
+//В этом коде функция makeOrderMessage объявляет три параметра orderedQuantity, pricePerDroid и deliveryFee. 
+//Внутри функции вычисляется общая стоимость заказа, умножая количество дроидов на цену одного дроида и добавляя стоимость доставки.
+// Затем создается переменная message, в которую записывается сообщение о заказе, используя синтаксис шаблонных строк.
+
+//После объявления функции makeOrderMessage, она вызывается с разными аргументами с помощью console.log(). 
+//Результаты вызовов выводятся в консоль в ожидаемом формате сообщения о заказе.
+
+
 
 
 
