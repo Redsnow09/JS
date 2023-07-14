ac007abd3e6af7d1c71dd798b4ccd6d5f4a5ec70
Redsnow09 <131536942+Redsnow09@users.noreply.github.com>
2023-07-14 22:26:26 +0300

    1

--- script.js
+++ script.js
@@ -1,10 +1,73 @@
-multiply(); 
-function multiply() {
+// 1. Объявление функции multiply
+//function sayHi() {
   // Тело функции
-  console.log("Hello, this is my first function!");
-}
+  //console.log("Hello, this is my first function!");
+//}
 
 // 2. Вызовы функции multiply
-multiply(sayHi); // multiply function invocation
-multiply(sayHi); // multiply function invocation
-multiply(sayHi); // multiply function invocation
+//sayHi(); // multiply function invocation
+//sayHi(); // multiply function invocation
+//sayHi(); // multiply function invocation
+
+
+
+//function add(a, b, c) {
+ // const result = a + b + c;
+  //console.log("Addition result equals " + result);
+//}
+
+//add(15, 27, 10);
+//add(10, 20, 30);
+//add(5, 10, 15);
+
+
+
+
+//function add(a, b, c) {
+  // Change code below this line
+//  return a + b + c;
+  // Change code above this line
+//}
+
+//add(2, 5, 8); // 15
+
+//console.log(add(15, 27, 10));
+//console.log(add(10, 20, 30));
+//console.log(add(5, 10, 15));
+
+
+
+
+//function calculateTotalPrice(orderedQuantity, pricePerItem) {
+  //let totalPrice = orderedQuantity * pricePerItem;
+  //return totalPrice;
+//}
+
+//console.log(calculateTotalPrice(5, 100)); // 500
+//console.log(calculateTotalPrice(8, 60)); // 480
+//console.log(calculateTotalPrice(3, 400)); // 1200
+//console.log(calculateTotalPrice(1, 3500)); // 3500
+//console.log(calculateTotalPrice(12, 70)); // 840
+
+//В этом коде функция calculateTotalPrice объявляет два параметра orderedQuantity и pricePerItem. Внутри функции создается переменная totalPrice, в которую записывается результат умножения orderedQuantity на pricePerItem. Затем функция возвращает значение переменной totalPrice.
+//После объявления функции calculateTotalPrice, она вызывается с разными аргументами с помощью console.log(). Результаты вызовов выводятся в консоль.
+
+
+
+
+function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+  let totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
+  let message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+  return message;
+}
+
+console.log(makeOrderMessage(2, 100, 50));
+console.log(makeOrderMessage(4, 300, 100));
+console.log(makeOrderMessage(10, 70, 200));
+
+
+
+
+
+
+
